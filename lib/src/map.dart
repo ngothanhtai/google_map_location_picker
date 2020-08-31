4b500455ce66e03e40df7702e910bcf6afcb3e77
Walter Ngo <codesigngo@gmail.com>
2020-08-31 12:52:11 +0700

    Update map.dart

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -395,7 +395,7 @@ class _MapFabs extends StatelessWidget {
 
     return Container(
       alignment: Alignment.topRight,
-      margin: const EdgeInsets.only(top: statusBarHeight + kToolbarHeight + 24, right: 8),
+      margin: EdgeInsets.only(top: statusBarHeight + kToolbarHeight + 24, right: 8),
       child: Column(
         children: <Widget>[
           if (layersButtonEnabled)
